8f9944f153f20c185f53c3b41f914278eba08f2a
Archangel Tyrael <90914809+HyperChaos1337@users.noreply.github.com>
2023-05-14 01:38:58 +0300

    Simplifying main

--- CW/main.cpp
+++ CW/main.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <conio.h>
 
+#define WINDOW_WIDTH 32
+#define WINDOW_HEIGHT 24
+
 #include "GameCore/cell.h"
 #include "SFML/Graphics.hpp"
 #include "GameCore/Field.h"
+#include "Entities/Player.h"
+#include "Control/Controller.h"
 
 const int H = 12;
-const int W = 12;
+const int W = WINDOW_WIDTH;
 
 using namespace sf;
 
 String Tile[H]{
-    "WWWWWWWWWWWW",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "W          W",
-    "WWWWWWWWWWWW"
+        "WWWWWWWWWWWWWWWWWWWWWWWW",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "W                      W",
+        "WWWWWWWWWWWWWWWWWWWWWWWW"
 };
-
 
 int main() {
 
     Cell cell;
-    RenderWindow window(VideoMode(cell.get_size() * H, cell.get_size() * W), "Test");
+    RenderWindow window(VideoMode(cell.get_size() * WINDOW_WIDTH, cell.get_size() * WINDOW_HEIGHT), "Test");
     /*
      t1 for Mario Texture
      t2 & t3 for Walls Texture
@@ -43,10 +47,8 @@ int main() {
      s2 & s3 for Walls Sprite
     */
     Sprite s1, s2, s3;
-    s1.setTexture(t1);
-    s1.setTextureRect(IntRect(70, 178, 50, 60));
-    s1.setPosition(150, 150);
-
+    Player player(t1);
+    Controller controller(&player);
     s2.setTexture(t2);
     s2.setTextureRect(IntRect(679, 170, cell.get_size(), cell.get_size()));
 
@@ -56,74 +58,17 @@ int main() {
 
     Clock clock;
 
-    float frame = 0;
-
     while (window.isOpen()) {
 
         float time = clock.getElapsedTime().asMicroseconds();
         clock.restart();
-
+        time = time/800;
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed) window.close();
         }
         window.clear(Color::Black);
-
-        if (Keyboard::isKeyPressed(Keyboard::A)) {
-            s1.move(-0.1, 0);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame), 178, 50, 70));
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::D)) {
-            s1.move(0.1, 0);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame) + 50, 178, -50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::W)){
-            s1.move(0, -0.1);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame), 178, 50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::S)){
-            s1.move(0, 0.1);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame) + 50, 178, -50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::A) && Keyboard::isKeyPressed(Keyboard::W)){
-            s1.move(-0.001, -0.001);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame), 178, 50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::A) && Keyboard::isKeyPressed(Keyboard::S)){
-            s1.move(-0.001, 0.001);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame), 178, 50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::D) && Keyboard::isKeyPressed(Keyboard::W)){
-            s1.move(0.001, -0.001);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame) + 50, 178, -50, 60));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::D) && Keyboard::isKeyPressed(Keyboard::S)){
-            s1.move(0.001, 0.001);
-            frame += 0.005;
-            if (frame > 3) frame -= 3;
-            s1.setTextureRect(IntRect(70 * int(frame) + 50, 178, -50, 60));
-        }
+        controller.control();
 
         for (int i = 0; i < H; i++)
             for (int j = 0; j < W; j++) {
@@ -133,8 +78,8 @@ int main() {
                 }
 
             }
-
-        window.draw(s1);
+        player.update(time);
+        window.draw(player.sprite);
         window.display();
     }
 
